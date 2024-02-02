59523116b3072a968db00aef6a3c109b63524fbb
Karl-Johan Bailey <karl@cenedex.com>
2024-02-02 18:01:19 -0400

    refactor: remove un needed code

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,19 @@ const DB = "";
     database: DB
   });
 
-
+  (await getAllTables(connection)).forEach(() => {
+    
+  })
 
 })()
 
 
-async function SQLQuery(sql: string, connection: mysql.Connection) {
+async function SQLQuery(sql: string, connection: mysql.Connection): Promise<[mysql.OkPacket | mysql.RowDataPacket[] | mysql.ResultSetHeader[] | mysql.RowDataPacket[][] | mysql.OkPacket[] | mysql.ProcedureCallPacket, mysql.FieldPacket[]]> {
     return await connection.query(sql);
 }
 
-async function getAllTables(connection: mysql.Connection) {
+async function getAllTables(connection: mysql.Connection): Promise<[mysql.OkPacket | mysql.RowDataPacket[] | mysql.ResultSetHeader[] | mysql.RowDataPacket[][] | mysql.OkPacket[] | mysql.ProcedureCallPacket, mysql.FieldPacket[]]> {
     const query = 'SELECT table_name FROM information_schema.tables WHERE table_schema = ?';
 
-    return connection.query(query, [connection.config.database])
+    return connection.query(query)
 }
